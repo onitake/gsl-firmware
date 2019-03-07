48dcdb80fcec20be618c75b5a40a98a2482f56f5
Van Laser <gabi_laser@yahoo.com>
2019-03-07 17:38:38 +0200

    fix and enhance rotate script

--- firmware/myria/my8307/auto-rotate.sh
+++ firmware/myria/my8307/auto-rotate.sh
@@ -5,31 +5,35 @@
 # automatically rotate the tablet *and* calibrate both touchscreen and
 # touchpad input (if keyboard is connected)
 
-
 TOUCHPAD="SIPODEV USB Composite Device Mouse"
-TOUCHSCREEN="Silead GSLx680 Touchscreen"
+
+# with gslx680_ts_acpi driver:
+#TOUCHSCREEN="Silead GSLx680 Touchscreen"
+# with silead_ts driver:
+TOUCHSCREEN="pointer:silead_ts"
 
 stdbuf -oL monitor-sensor |
 	while IFS= read -r line; do
 		if [ -z "${line##*right*}" ]; then
-			xrandr -o left --dpi 96
+			xrandr -o left
 			xinput set-prop "$TOUCHPAD"     --type=float "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
 			xinput set-prop "$TOUCHSCREEN"  --type=float "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
 		fi
 		if [ -z "${line##*normal*}" ]; then
-			xrandr -o normal --dpi 96
+			xrandr -o normal
 			xinput set-prop "$TOUCHPAD"     --type=float "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
 			xinput set-prop "$TOUCHSCREEN"  --type=float "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
 		fi
 		if [ -z "${line##*left*}" ]; then
-			xrandr -o right --dpi 96
+			xrandr -o right
 			xinput set-prop "$TOUCHPAD"     --type=float "Coordinate Transformation Matrix" -1 0 1 0 -1 1 0 0 1
 			xinput set-prop "$TOUCHSCREEN"  --type=float "Coordinate Transformation Matrix" -1 0 1 0 -1 1 0 0 1
 		fi
 		if [ -z "${line##*bottom*}" ]; then
-			xrandr -o inverted --dpi 96
+			xrandr -o inverted
 			xinput set-prop "$TOUCHPAD"     --type=float "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
 			xinput set-prop "$TOUCHSCREEN"  --type=float "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
 		fi
+		xrandr --dpi 96
 	done
 
